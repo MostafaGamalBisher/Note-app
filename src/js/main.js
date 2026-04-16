7bcf9b8cd63f444aa27bdeef618148b3372bcc67
mostafagamalbisher <mostafagamalbisher@outlook.com>
2026-04-16 10:16:13 +0300

    refactor: add UI module for DOM element references and remove unused navigation code

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,35 @@
 import '../scss/main.scss';
 
-const allNavItems = document.querySelectorAll('.nav-menu__item');
-const app = document.querySelector('.app');
+// const allNavItems = document.querySelectorAll('.nav-menu__item');
+// const app = document.querySelector('.app');
 
-allNavItems.forEach((item) => {
-  item.addEventListener('click', (e) => {
-    e.preventDefault();
+// allNavItems.forEach((item) => {
+//   item.addEventListener('click', (e) => {
+//     e.preventDefault();
 
-    // Get the text of the clicked link
-    const clickedLinkText = item
-      .querySelector('.nav-menu__link')
-      .textContent.trim();
+//     // Get the text of the clicked link
+//     const clickedLinkText = item
+//       .querySelector('.nav-menu__link')
+//       .textContent.trim();
 
-    // Remove active from ALL
-    allNavItems.forEach((navItem) => {
-      navItem.classList.remove('nav-menu__item--active');
+//     // Remove active from ALL
+//     allNavItems.forEach((navItem) => {
+//       navItem.classList.remove('nav-menu__item--active');
 
-      // Add active to matching items in BOTH menus
-      const navLinkText = navItem
-        .querySelector('.nav-menu__link')
-        .textContent.trim();
-      if (navLinkText === clickedLinkText) {
-        navItem.classList.add('nav-menu__item--active');
-      }
-    });
+//       // Add active to matching items in BOTH menus
+//       const navLinkText = navItem
+//         .querySelector('.nav-menu__link')
+//         .textContent.trim();
+//       if (navLinkText === clickedLinkText) {
+//         navItem.classList.add('nav-menu__item--active');
+//       }
+//     });
 
-    // Update data-view
-    if (clickedLinkText === 'Add Notes') {
-      app.setAttribute('data-view', 'add-note');
-    } else {
-      app.setAttribute('data-view', 'notes');
-    }
-  });
-});
+//     // Update data-view
+//     if (clickedLinkText === 'Add Notes') {
+//       app.setAttribute('data-view', 'add-note');
+//     } else {
+//       app.setAttribute('data-view', 'notes');
+//     }
+//   });
+// });
---
+++ src/js/ui.js
@@ -0,0 +1,12 @@
+export const DOM = {
+  app: document.querySelector('.app'),
+
+  menuOverLay: document.querySelector('#menu-overlay'),
+  searchOverLay: document.querySelector('#search-overlay'),
+
+  menuOpenBtn: document.querySelector('.mobile-header__burger'),
+  searchOpenBtn: document.querySelector('.mobile-header__search-btn'),
+
+  menuCloseBtn: document.querySelector('.menu-overlay__close'),
+  searchCloseBtn: document.querySelector('.search-overlay__close'),
+};
